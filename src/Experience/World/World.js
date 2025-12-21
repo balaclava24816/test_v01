8b4307c8dcd790cb949fde89241ea78145b64087
balaclava24816 <balaclava24816@proton.me>
2025-12-22 00:04:54 +0700

    1

---
+++ src/Experience/World/World.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three'
+import Experience from "../Experience.js"
+import Environment from './Environment.js'
+import Floor from './Floor.js'
+
+export default class World
+{
+    constructor()
+    {
+        this.experience = new Experience()
+        this.scene = this.experience.scene
+        this.resources = this.experience.resources
+
+        // Test mesh
+        const testMesh = new THREE.Mesh(
+            new THREE.BoxGeometry(1, 1, 1),
+            new THREE.MeshStandardMaterial()
+        )
+        this.scene.add(testMesh)
+
+        // Wait for resources
+        this.resources.on('ready', () =>
+        {
+            // Setup
+            this.floor = new Floor()
+            this.environment = new Environment()
+            
+        })
+        
+    }
+}
